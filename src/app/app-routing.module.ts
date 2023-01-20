73c90079bbbd8d39900f19a70e770bcf70f4dd9c
sidneyrdm <sidneybritomal@gmail.com>
2023-01-19 23:03:46 -0300

    implementação da página de contato

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { DetalheProdutoComponent } from './produtos/detalhe-produto/detalhe-prod
 const routes: Routes = [{ path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
 {path: "", redirectTo: "produtos", pathMatch: "full"},
 { path: 'carrinho', loadChildren: () => import('./carrinho/carrinho.module').then(m => m.CarrinhoModule) },
+{ path: 'contato', loadChildren: () => import('./contato/contato.module').then(m => m.ContatoModule) },
 {path: "**", component: PaginaNaoEncontradaComponent}
 ];
 
